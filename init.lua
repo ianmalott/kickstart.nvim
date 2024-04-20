55cf697709990806275c785fd4337986ed976bf8
Ian Malott <ian@ianmalott.com>
2024-04-19 20:06:14 -0400

    Add file position percentage to statusline

--- init.lua
+++ init.lua
@@ -947,7 +947,7 @@ require('lazy').setup({
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return '%2p%% %2l:%-2v'
       end
 
       -- ... and there is more!
