b27a74820b51297a8bfdab039bc7aa72f24eb09f
Ian Malott <ian@ianmalott.com>
2024-04-20 20:46:36 -0400

    Conform.nvim: use mix as elixir formatter

--- init.lua
+++ init.lua
@@ -783,6 +783,7 @@ require('lazy').setup({
         end
       end,
       formatters_by_ft = {
+        elixir = { 'mix' },
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
