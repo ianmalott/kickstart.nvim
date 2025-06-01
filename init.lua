730ba36e2ea3a31322204c93232f784a7d498898
Ian Malott <ian@ianmalott.com>
2025-06-01 09:37:36 -0400

    Address warning: missing required field

--- init.lua
+++ init.lua
@@ -736,6 +736,7 @@ require('lazy').setup({
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
+        automatic_enable = {}, -- required field in type MasonLspconfigSettings
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
         automatic_installation = false,
         handlers = {
